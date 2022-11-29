47fef2a20016f3aa5725791e9a4b13365c0882a3
franek86 <frane.konjevoda@gmail.com>
2022-11-29 21:07:07 +0100

    Create get single transaction api router

--- routes/transaction.js
+++ routes/transaction.js
@@ -4,6 +4,7 @@ const transactionController = require("../controllers/transaction");
 
 router.post("/", transactionController.createTransaction);
 router.get("/", transactionController.getAllTransaction);
+router.get("/:id", transactionController.getSingleTransaction);
 router.patch("/:id", transactionController.updateTransaction);
 router.delete("/:id", transactionController.deleteTransaction);
 
--- server.js
+++ server.js
@@ -2,27 +2,22 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv").config();
-const errorHander = require("./middleware/errorHandler")
+const errorHander = require("./middleware/errorHandler");
 const port = process.env.PORT || 5000;
 const connectDB = require("./database/db");
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 
-app.use(cors())
+app.use(cors());
 
 //call connect to databsae
 connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
 const categoryRoute = require("./routes/category");
 const transactionRoute = require("./routes/transaction");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
-
-
-
 
 app.use("/api/category", categoryRoute);
 app.use("/api/transaction", transactionRoute);
@@ -31,7 +26,6 @@ app.use("/api/user", userRoute);
 app.use(errorHander);
 app.use(cookieParser);
 
-
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-})
+  console.log(`Listening on port ${port}`);
+});
