633ab5870ee0d9fecef2db56d78db98933dbc885
franek86 <frane.konjevoda@gmail.com>
2023-03-19 19:12:36 +0100

    add legend color field

--- models/Category.js
+++ models/Category.js
@@ -1,22 +1,23 @@
 const mongoose = require("mongoose");
 
-const CategorySchema = new mongoose.Schema(
-    {
-        name:{
-            type:String,
-            require:true
-        },
-        slug:{
-            type:String,
-            require:true
-        },
-        user:{
-            type:mongoose.Schema.Types.ObjectId,
-            require:true,
-            ref:"User"
-        }
-        
-    }
-)
+const CategorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  slug: {
+    type: String,
+    require: true,
+  },
+  legendColor: {
+    type: String,
+    require: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    require: true,
+    ref: "User",
+  },
+});
 
 module.exports = mongoose.model("Category", CategorySchema);
